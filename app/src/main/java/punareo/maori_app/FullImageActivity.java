32fb397fd5233fe8b7749e6359cfd176ae81e21e
Rhyan <daedem@windowslive.com>
2014-08-26 12:31:09 +1200

    Working swipe controls

--- app/src/main/java/punareo/maori_app/FullImageActivity.java
+++ app/src/main/java/punareo/maori_app/FullImageActivity.java
@@ -7,15 +7,31 @@ package punareo.maori_app;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.GestureDetector;
+import android.widget.TextView;
+import android.view.MotionEvent;
+import android.os.Bundle;
+import android.view.GestureDetector;
 import android.widget.ImageView;
-
+import android.support.v4.view.GestureDetectorCompat;
 
 public class FullImageActivity extends Activity
+    implements GestureDetector.OnGestureListener,
+                GestureDetector.OnDoubleTapListener
 {
+    private TextView gestureText;
+    private GestureDetectorCompat gDetector;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.full_image);
+
+        gestureText = (TextView)findViewById(R.id.gestureStatusText);
+        this.gDetector = new GestureDetectorCompat(this, this);
+        gDetector.setOnDoubleTapListener(this);
 
         // get intent data
         Intent i = getIntent();
@@ -24,7 +40,71 @@ public class FullImageActivity extends Activity
         int position = i.getExtras().getInt("id");
         ImageAdapter imageAdapter = new ImageAdapter(this);
 
-        ImageView imageView = (ImageView) findViewById(R.id.imageview_body);
+        ImageView imageView = (ImageView) findViewById(R.id.full_image_view);
         imageView.setImageResource(imageAdapter.mThumbIds[position]);
+    }
+
+
+
+    //Required Gesture Detection Methods
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        this.gDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent event) {
+        gestureText.setText ("onDown");
+        return true;
+    }
+
+    @Override
+    public boolean onFling(MotionEvent event1, MotionEvent event2,
+                           float velocityX, float velocityY) {
+        gestureText.setText("onFling");
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent event) {
+        gestureText.setText("onLongPress");
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2,
+                            float distanceX, float distanceY) {
+        gestureText.setText("onScroll");
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent event) {
+        gestureText.setText("onShowPress");
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent event) {
+        gestureText.setText("onSingleTapUp");
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent event) {
+        gestureText.setText("onDoubleTap");
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent event) {
+        gestureText.setText("onDoubleTapEvent");
+        return true;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent event) {
+        gestureText.setText("onSingleTapConfirmed");
+        return true;
     }
 }
---
+++ app/src/main/java/punareo/maori_app/XMLParser.java
@@ -0,0 +1,19 @@
+package punareo.maori_app;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.List;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+/**
+ * Created by 21002282 on 25/08/2014.
+ */
+
+
+public class XMLParser
+{
+    private static final String ns = null;
+}
